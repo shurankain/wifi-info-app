f01c4fed140701fa0bd6fbfb3c9c89ed6b019ea5
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-13 13:04:23 +0100

    basic working implementatioon, no styling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,34 @@
+use std::process::Command;
+use log::info;
+
+#[tauri::command]
+async fn get_wifi_data() -> Result<String, String> {
+    info!("Getting wifi data");
+    let output = Command::new("system_profiler")
+        .arg("SPAirPortDataType")
+        .output();
+
+    let res = match output {
+        Ok(output) if output.status.success() => {
+            std::str::from_utf8(&output.stdout).unwrap_or("Invalid UTF-8 output").to_string()
+        }
+        Ok(output) => {
+            output.status.to_string()
+        }
+        Err(e) => {
+            e.to_string()
+        }
+    };
+
+    Ok(res)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![get_wifi_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
+// import { info } from '@tauri-apps/plugin-log';
 
 function App() {
 
+ const [wifiData, setWifiData] = useState("Loading...");
+
+ useEffect(() => {
+    async function loadWifiData() {
+      try {
+        // info("Trying to load wifi data from Rust");
+        let loadingResult = await invoke("get_wifi_data");
+        // info("Wifi data loaded: " + loadingResult);
+        setWifiData(loadingResult);
+      } catch (error) {
+        setWifiData("error during data retrieval");
+      }
+    }
+    loadWifiData();
+  }, []);
+
   return (
     <main className="container">
-      <h1>wifi-info-placeholder</h1>
+      <h1>{wifiData}</h1>
     </main>
   );
 }
