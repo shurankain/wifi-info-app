f16dcdd4b778d40087d5e3ed9f4d902ed48a5f76
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-14 21:37:14 +0100

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/core";
-// import { info } from '@tauri-apps/plugin-log';
+import "./App.css";
+import { info } from "@tauri-apps/plugin-log";
 
 function App() {
 
- const [wifiData, setWifiData] = useState("Loading...");
+    const [wifiData, setWifiData] = useState("Loading...");
 
- useEffect(() => {
-    async function loadWifiData() {
-      try {
-        // info("Trying to load wifi data from Rust");
-        let loadingResult = await invoke("get_wifi_data");
-        // info("Wifi data loaded: " + loadingResult);
-        setWifiData(loadingResult);
-      } catch (error) {
-        setWifiData("error during data retrieval");
-      }
-    }
-    loadWifiData();
-  }, []);
+    useEffect(() => {
+        async function loadWifiData() {
+            try {
+                let loadingResult = await invoke("get_wifi_data");
+                info("Wifi data loaded: " + loadingResult);
+                setWifiData(loadingResult);
+            } catch (error) {
+                setWifiData("error during data retrieval");
+            }
+        }
+        loadWifiData();
+    }, []);
 
-  return (
-    <main className="container">
-      <h1>{wifiData}</h1>
-    </main>
-  );
+    return (
+        <main className="container">
+            <h1>{wifiData}</h1>
+        </main>
+    );
 }
 
 export default App;
